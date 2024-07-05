7c4a351350cd064ceb216e097a0fdfbb237fdad2
AndreaIoanaManea <160650399+AndreaIoanaManea@users.noreply.github.com>
2024-07-05 11:37:26 +0200

    buttonsc.js

--- buttonsc.js
+++ buttonsc.js
@@ -1,9 +1,7 @@
- // Funcția care adaugă evenimentul de scroll și decide când să afișeze butonul
  window.addEventListener("scroll", function () {
     var scrollToTopBtn = document.getElementById("scrollToTopBtn");
     var halfViewportHeight = window.innerHeight / 2;
 
-    // Dacă utilizatorul a derulat mai mult de jumătatea viewport-ului, afișează butonul
     if (window.scrollY > halfViewportHeight) {
         scrollToTopBtn.style.display = "block";
     } else {
@@ -11,7 +9,6 @@
     }
 });
 
-// Funcția care rulează când utilizatorul face clic pe buton pentru a derula sus
 function scrollToTop() {
     window.scrollTo({
         top: 0,
