d99de09b89a2b53037fcc2ee80c28a69c053ee06
AndreaIoanaManea <160650399+AndreaIoanaManea@users.noreply.github.com>
2024-07-05 11:38:33 +0200

    cluj-napoca.js

--- cluj-napoca.js
+++ cluj-napoca.js
@@ -1,13 +1,8 @@
 const currentCityTag = document.querySelector(".current-city");
 let currentCity = localStorage.getItem("city");
 
+currentCity="Cluj";
 
-
-   
-    currentCity="Cluj";
-
-
-//actualizam orasul afisat pe ecran
 currentCityTag.innerHTML = currentCity;
 
 
