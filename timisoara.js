207957c57a99239686cba67eaae09bbccbb825c9
AndreaIoanaManea <160650399+AndreaIoanaManea@users.noreply.github.com>
2024-07-05 11:52:41 +0200

    timisoara.js

--- timisoara.js
+++ timisoara.js
@@ -1,14 +1,8 @@
 const currentCityTag = document.querySelector(".current-city");
 let currentCity = localStorage.getItem("city");
 
-
-
-
 currentCity="Timișoara";
 
-
-
-//actualizam orasul afisat pe ecran
 currentCityTag.innerHTML = currentCity;
 
 
