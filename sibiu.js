71ebfffb5f78c54efc43b085a1bd257822c9f228
AndreaIoanaManea <160650399+AndreaIoanaManea@users.noreply.github.com>
2024-07-05 11:50:52 +0200

    sibiu.js

--- sibiu.js
+++ sibiu.js
@@ -1,13 +1,8 @@
 const currentCityTag = document.querySelector(".current-city");
 let currentCity = localStorage.getItem("city");
 
+currentCity="Sibiu";
 
-
-  
-    currentCity="Sibiu";
-
-
-//actualizam orasul afisat pe ecran
 currentCityTag.innerHTML = currentCity;
 
 
